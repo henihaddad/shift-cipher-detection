98843ddfb6c675b543b3ab70e2ab908fabc07e85
Hani Haddad <83586321+henihaddad@users.noreply.github.com>
2024-01-04 20:15:14 +0100

    Update main.cpp
    Made the code cleaner with the same logic since the other version I made it when I 16 years old.

--- main.cpp
+++ main.cpp
@@ -1,136 +1,81 @@
 #include <iostream>
 #include <cctype>
 #include <string>
+#include <array>
+#include <vector>
+
 using namespace std;
-int scoreParDecalage[26];
 
-bool test2(string ch)
-{
-    bool a = false;
-    int n = 14;
-    string motFrequenteA2Lettre[n] = {"le", "la", "il", "me", "te", "se", "ma", "si", "je", "on", "de", "un", "du", "ce"};
-    for (int i = 0; i < n; i++)
-    {
-        if (ch == motFrequenteA2Lettre[i])
-            a = true;
+array<int, 26> scoreParDecalage = {};
+
+bool testMot(const string& ch, const vector<string>& motsFrequents) {
+    for (const auto& mot : motsFrequents) {
+        if (ch == mot) {
+            return true;
+        }
     }
-    return a;
-}
-bool test4(string ch)
-{
-    bool a = false;
-    int n = 6;
-    string motFrequenteA4Lettre[n] = {"elle", "celle", "ceux", "plus", "onze", "quel"};
-    for (int i = 0; i < n; i++)
-    {
-        if (ch == motFrequenteA4Lettre[i])
-            a = true;
-    }
-    return a;
-}
-bool test3(string ch)
-{
-    bool a = false;
-    int n = 5;
-    string motFrequenteA3Lettre[n] = {
-        "pas",
-        "son",
-        "mon",
-        "nos",
-        "une",
-    };
-    for (int i = 0; i < n; i++)
-    {
-        if (ch == motFrequenteA3Lettre[i])
-            a = true;
-    }
-    return a;
-}
-bool testTout(string mot)
-{
-    int l = mot.length();
-    if (l == 2)
-        return test2(mot);
-    else if (l == 1)
-        return (mot == "a");
-    else if (l == 3)
-        return test3(mot);
-    else if (l == 4)
-        return test4(mot);
-    else
-        return false;
+    return false;
 }
 
-string decodageParDecalageMot(string mot, int dec)
-{
-    for (int i = 0; i < mot.length(); i++)
-    {
-        mot[i] = tolower(mot[i]);
-        mot[i] = (((mot[i] + dec - 'a') % 26) + 26) % 26 + 'a';
+bool testTout(const string& mot) {
+    static const vector<string> mots2Lettres = {"le", "la", "il", "me", "te", "se", "ma", "si", "je", "on", "de", "un", "du", "ce"};
+    static const vector<string> mots3Lettres = {"pas", "son", "mon", "nos", "une"};
+    static const vector<string> mots4Lettres = {"elle", "celle", "ceux", "plus", "onze", "quel"};
+
+    switch (mot.length()) {
+        case 1: return mot == "a";
+        case 2: return testMot(mot, mots2Lettres);
+        case 3: return testMot(mot, mots3Lettres);
+        case 4: return testMot(mot, mots4Lettres);
+        default: return false;
     }
-    return mot;
 }
-string decodageParDecalageTout(string texte, int dec)
-{
-    for (int i = 0; i < texte.length(); i++)
-    {
-        char c = texte[i];
-        if (isalpha(c))
-        {
+
+string decodageParDecalage(const string& mot, int dec) {
+    string result = mot;
+    for (char& c : result) {
+        if (isalpha(c)) {
             bool isMaj = isupper(c);
             c = tolower(c);
-            c = (((c + dec - 'a') % 26) + 26) % 26 + 'a';
-            if (isMaj)
+            c = (((c - 'a' + dec + 26) % 26) + 'a');
+            if (isMaj) {
                 c = toupper(c);
-        }
-        texte[i] = c;
-    }
-    return texte;
-}
-int meilleurDecalage()
-{
-    int max = -1, k = -1;
-    for (int i = 0; i < 26; i++)
-    {
-        if (scoreParDecalage[i] > max)
-        {
-            max = scoreParDecalage[i];
-            k = i;
+            }
         }
     }
-    return k;
+    return result;
 }
-int main()
-{
-    for (int i = 0; i < 26; i++)
-        scoreParDecalage[i] = 0;
-    string texte;
+
+int meilleurDecalage() {
+    int maxVal = -1, meilleurDec = -1;
+    for (int i = 0; i < 26; i++) {
+        if (scoreParDecalage[i] > maxVal) {
+            maxVal = scoreParDecalage[i];
+            meilleurDec = i;
+        }
+    }
+    return meilleurDec;
+}
+
+int main() {
+    string texte, mot;
     getline(cin, texte);
-    texte += " ";
-    string mot = "";
-    for (int i = 0; i < texte.length(); i++)
-    {
 
-        if (isalpha(texte[i]))
-        {
-            mot += texte[i];
-        }
-        else
-        {
-
-            for (int dec = 0; dec < 26; dec++)
-            {
-                string aTester = decodageParDecalageMot(mot, dec);
-                if (testTout(aTester))
-                {
+    for (char c : texte) {
+        if (isalpha(c)) {
+            mot += c;
+        } else if (!mot.empty()) {
+            for (int dec = 0; dec < 26; dec++) {
+                if (testTout(decodageParDecalage(mot, dec))) {
                     scoreParDecalage[dec]++;
                 }
             }
-            mot = "";
+            mot.clear();
         }
     }
-    int meilleurDec = meilleurDecalage();
-    string theBest = decodageParDecalageTout(texte, meilleurDec);
 
-    cout << theBest;
+    string texteDecode = decodageParDecalage(texte, meilleurDecalage());
+    cout << texteDecode;
+
+    return 0;
 }
